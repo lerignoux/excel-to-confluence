781d884c5268f338acf6050f4fcadf21168badbf
Laurent Erignoux <lerignoux@gmail.com>
2019-02-11 00:28:23 +0800

    Fixing confluence code

--- app/confluence.py
+++ app/confluence.py
@@ -17,11 +17,10 @@ class Confluence():
     def host(self):
         return self.config("confluence", {}).get('host')
 
-    def source_from_data(self, data, , header="", template="default.html"):
+    def source_from_data(self, data, header="", template="default.html"):
         with open(f"confluence_templates/{template}") as f:
             res = f.read()
-        tables_source =
-        res.format(header=header, table=self.table_from_data(data))
+        tables_source = res.format(header=header, table=self.table_source_from_data(data))
 
     def table_source_from_data(self, data, template="default_table.html"):
         with open(f"confluence_templates/{template}") as f:
@@ -31,9 +30,9 @@ class Confluence():
             rows = []
             for row_data in data:
                 rows.append("\n".join(
-                    (cell_tpl.format(style=cell.get(style, ""), value=cell.get(value, "")) for cell in row_data)
+                    (self.cell_tpl.format(style=cell.get('style', ""), value=cell.get('value', "")) for cell in row_data)
                 ))
             tables.append(deepcopy(tpl).format(content="\n".join(
-                row_tpl.format(style="", value=row) for row in rows
+                self.row_tpl.format(style="", value=row) for row in rows
             )))
         return "\n".join(table for table in tables)
